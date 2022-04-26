7060d4f41cb28ef6e07c2f19803a069dbcafb197
TNattawutK <tomkabtokom@gmail.com>
2022-04-26 18:57:54 +0700

    fix: from class to className

--- src/components/ThemeSwitch/index.jsx
+++ src/components/ThemeSwitch/index.jsx
@@ -16,18 +16,20 @@ function ThemeSwitch() {
         className="p-2 space-y-2 overflow-y-auto rounded shadow dropdown-content menu bg-base-200 w-44 max-h-64"
       >
         {themes.map((theme) => (
-          <li data-theme={theme} className="rounded">
+          <li data-theme={theme} className="rounded" key={theme}>
             <button
               data-set-theme={theme}
               data-act-class="outline-base-content"
               className="!rounded p-2 bg-base-100 text-base-content capitalize"
             >
-              <div class="flex-grow text-sm font-bold text-left">{theme}</div>
-              <div class="flex flex-wrap flex-shrink-0 gap-1">
-                <div class="w-2 h-2 rounded bg-primary" />
-                <div class="w-2 h-2 rounded bg-secondary" />
-                <div class="w-2 h-2 rounded bg-accent" />
-                <div class="w-2 h-2 rounded bg-neutral" />
+              <div className="flex-grow text-sm font-bold text-left">
+                {theme}
+              </div>
+              <div className="flex flex-wrap flex-shrink-0 gap-1">
+                <div className="w-2 h-2 rounded bg-primary" />
+                <div className="w-2 h-2 rounded bg-secondary" />
+                <div className="w-2 h-2 rounded bg-accent" />
+                <div className="w-2 h-2 rounded bg-neutral" />
               </div>
             </button>
           </li>
